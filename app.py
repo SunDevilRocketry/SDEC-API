f4ee50c0ec127f37bf9911ff9560b3ea1fb4b363
niekky <trungkiencfc@gmail.com>
2025-02-14 15:07:36 -0700

    commit

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # save this as app.py
 from flask import Flask
 
+from sdec import sdec
+
+terminalSerObj = sdec.terminalData()
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/comport-l")
+def comport():
+    userCommand = "comport"
+    userArgs = "-l"
+    terminalSerObj, ports = sdec.command_list[userCommand](userArgs, terminalSerObj)
+    return ports
 
 
 if __name__ == '__main__':
