c9758d8290f24fb7a059d1cdaa27defefadea078
journeyhancock <journeyhancock480@gmail.com>
2025-02-14 15:06:52 -0700

    fixed imports

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
+# General
+import time
+import os
+import sys
+import datetime
+import math
+
 # save this as app.py
 from flask import Flask
+
+# Setup path
+sys.path.insert(0, './plumbing')
+sys.path.insert(0, './sdec')
+
+# SDEC
+import sdec
+import commands
+import hw_commands
+import sensor_conv
+import engineController
 
 app = Flask(__name__)
 
