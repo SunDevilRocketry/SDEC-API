50294a6478d2cac9181fd8643bf8b4d38517bfe9
Harsh <14aug2004@gmail.com>
2025-03-21 14:55:03 -0700

    implemented sensor_get

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ sys.path.insert(0, './sdec')
 # SDEC
 import sdec
 
-from sdec import sdec
+#from sdec import sdec
 
 terminalSerObj = sdec.terminalData()
 
@@ -86,6 +86,14 @@ def sensor_poll():
     # \?time=<time>
     time = int(request.args.get('time', 100)) # Defaults to 100 sensor-dumps
     return Response(sensor_poll_dump(time), mimetype='text/event-stream')
+
+@app.route("/sensor-get", methods=['Get'])
+def sensor_get():
+    global terminalSerObj
+    userCommand = "sensor"
+    userArgs = ["dump"]
+    terminalSerObj, sensor_data = sdec.command_list[userCommand](userArgs, terminalSerObj)
+    return sensor_data
 
 @app.route("/")
 def default():
