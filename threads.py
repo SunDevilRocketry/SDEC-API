f0981a98b5ca5c901c365d58f22d1d040e43bdfc
ETSsound <147210601+ETSsound@users.noreply.github.com>
2026-04-14 14:06:52 -0700

    feat: Initial dashboard dump attempt (GS/FC cross platform)

--- threads.py
+++ threads.py
@@ -5,28 +5,20 @@ import threading
 import time
 
 from hardware import serial_lock
-from SDECv2 import SerialObj, SensorSentry
+from SDECv2 import SerialObj, SensorSentry, Telemetry
 from typing import Dict
 from util import make_safe_number
 
 POLL_INTERVAL = 1/60 # In seconds, target is 60 Hz
 
-def poll_dashboard_dump(serial_connection: SerialObj, data_dict, stop_event: threading.Event):
+def poll_dashboard_dump(serial_connection: SerialObj, stop_event: threading.Event, telemetry_obj: Telemetry):
     next_time = time.perf_counter()
    
     while not stop_event.is_set():
         next_time += POLL_INTERVAL
 
         with serial_lock():
-            dashboard_dump = SensorSentry.dashboard_dump(serial_connection)
-
-        curr_dashboard_dump = {}
-        for sensor, readout in dashboard_dump.items():
-            val = make_safe_number(readout)
-            curr_dashboard_dump[sensor.short_name] = val
-
-        data_dict.clear()
-        data_dict.update(curr_dashboard_dump)
+            telemetry_obj.dashboard_dump(serial_connection)
 
         sleep_time = next_time - time.perf_counter()
         if sleep_time > 0: 
